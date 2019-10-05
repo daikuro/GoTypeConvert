14559728423bc9a5328b0129082d00f9eee05e48
daikuro <daikuro@users.noreply.github.com>
2019-10-05 17:52:06 +0900

    add test

--- int_test.go
+++ int_test.go
@@ -1,9 +1,37 @@
 package typeconv
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Example() {
+func ExampleInt1() {
 	fmt.Println(ToInt(1, 0).A)
 	// Output:
 	// 1
+}
+
+func ExampleInt2() {
+	fmt.Println(ToInt("2", 0).A)
+	// Output:
+	// 2
+}
+
+func ExampleInt3() {
+	fmt.Println(ToInt(-1, 0).A)
+	// Output:
+	// -1
+}
+
+func ExampleInt4() {
+	fmt.Println(ToInt(1.1, 0).A)
+	// Output:
+	// 1
+}
+
+func ExampleInt5() {
+	fmt.Println(ToInt("e", 0).A)
+	fmt.Println(ToInt("e", 0).Error)
+	// Output:
+	// 0
+	// strconv.Atoi: parsing "e": invalid syntax
 }
