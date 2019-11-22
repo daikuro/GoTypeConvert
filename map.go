c1b4b8c66a91a1712481a805b9d9c02726e1b61d
daikuro <daikuro@users.noreply.github.com>
2019-11-23 08:09:55 +0900

    interface to map

--- map.go
+++ map.go
@@ -1,9 +1,52 @@
 package typeconv
 
+import (
+	"reflect"
+)
+
 func ToMap(value interface{}) map[string]interface{} {
+	if value == nil {
+		return nil
+	}
 	switch t := value.(type) {
 	case map[string]interface{}:
 		return t
 	}
-	return nil
+	if reflect.Indirect(reflect.ValueOf(value)).Kind() != reflect.Struct {
+		return nil
+	}
+	return toMap(value)
+}
+
+func toMap(b interface{}) map[string]interface{} {
+	if b == nil {
+		return nil
+	}
+	t := reflect.TypeOf(b).Elem()
+	v := reflect.ValueOf(b).Elem()
+	num := t.NumField()
+	m := map[string]interface{}{}
+	for i := 0; i < num; i++ {
+		field := t.Field(i)
+		vf := v.Field(i)
+		if !vf.CanSet() { // private variable
+			continue
+		}
+		name := field.Name
+		vi := vf.Interface()
+		if vi == nil {
+			continue
+		}
+		vii := reflect.Indirect(reflect.ValueOf(vi))
+		if vii.Kind() != reflect.Struct {
+			if !vii.IsValid() { // skip if value is nil
+				continue
+			}
+			m[name] = vi
+			continue
+		}
+		vi = toMap(vi)
+		m[name] = vi
+	}
+	return m
 }
--- map_test.go
+++ map_test.go
@@ -11,3 +11,85 @@ func ExampleToMap() {
 	// Output:
 	// map[]
 }
+
+func ExampleToMapInt() {
+	d := map[string]interface{}{
+		"A": 111,
+	}
+	a := ToMap(d)
+	fmt.Println(a)
+	// Output:
+	// map[A:111]
+}
+
+func ExampleToMapStruct() {
+	type User struct {
+		Name string
+	}
+	user := &User{
+		Name: "TestName",
+	}
+
+	a := ToMap(user)
+	fmt.Println(a)
+	// Output:
+	// map[Name:TestName]
+}
+
+func ExampleToMapStructFields() {
+	type User struct {
+		Name string
+		Id   int
+	}
+	user := &User{
+		Name: "TestName",
+		Id:   100,
+	}
+
+	a := ToMap(user)
+	fmt.Println(a["Id"], a["Name"])
+	// Output:
+	// 100 TestName
+}
+
+func ExampleToMapStructNest() {
+	type User struct {
+		Name    string
+		SubUser *User
+	}
+	user := &User{
+		Name: "TestName",
+		SubUser: &User{
+			Name: "SubName",
+		},
+	}
+
+	a := ToMap(user)
+	//fmt.Println(a["SubUser"].(*User).Name)
+	fmt.Println(a["Name"], a["SubUser"])
+	// Output:
+	// TestName map[Name:SubName]
+}
+
+func ExampleToMapStructPrivateField() {
+	type User struct {
+		Name string
+		id   int
+	}
+	user := &User{
+		Name: "TestUser",
+		id:   100,
+	}
+
+	a := ToMap(user)
+	fmt.Println(a)
+	// Output:
+	// map[Name:TestUser]
+}
+
+func ExampleToMapString() {
+	a := ToMap("aaa")
+	fmt.Println(a)
+	// Output:
+	// map[]
+}
